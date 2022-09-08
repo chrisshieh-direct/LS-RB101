b8b7268c261ab73e29e200b456ad02a29adf7d72
Chris Shieh <dev@chrisgrace.com>
2022-09-08 15:45:23 -0700

    cleaned up language

---
+++ lesson_2/6.rb
@@ -0,0 +1,33 @@
+puts "Give me a number, please."
+num1 = gets.to_i
+
+puts "What's the second number?"
+num2 = gets.to_i
+
+options = ["a","s","m","d"]
+op = nil
+
+loop do
+puts "Would you like to add, subtract, multiply, or divide? (A/S/M/D)"
+op = gets.chomp.downcase
+if options.include?(op)
+  break
+else
+  puts "That's not one of the choices. Please type A, M, S, or D."
+end
+end
+
+result = nil
+
+case op
+when "a"
+  result = num1 + num2
+when "s"
+  result = num1 - num2
+when "m"
+  result = num1 * num2
+else
+  result = num1.to_f / num2.to_f
+end
+
+puts result
