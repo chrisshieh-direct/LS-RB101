99cad19a7683bfc3c0150dca5204ec9fff95aea0
Chris Shieh <chrisshieh.direct@gmail.com>
2022-09-28 18:55:42 -0600

    keeping progression

--- lesson_6/tic2.rb
+++ lesson_6/tic2.rb
@@ -1,17 +1,39 @@
-def display_board(h)
+def display_board(a)
+  system('clear')
   puts '-------------'
-  puts '| ' + cell(h[:tl]) + ' | ' + cell(h[:tm]) + ' | ' + cell(h[:tr]) + ' |'
+  puts '| ' + cell(a[0]) + ' | ' + cell(a[1]) + ' | ' + cell(a[2]) + ' |'
   puts '-------------'
-  puts '| ' + cell(h[:ml]) + ' | ' + cell(h[:mm]) + ' | ' + cell(h[:ml]) + ' |'
+  puts '| ' + cell(a[3]) + ' | ' + cell(a[4]) + ' | ' + cell(a[5]) + ' |'
   puts '-------------'
-  puts '| ' + cell(h[:bl]) + ' | ' + cell(h[:bm]) + ' | ' + cell(h[:bl]) + ' |'
+  puts '| ' + cell(a[6]) + ' | ' + cell(a[7]) + ' | ' + cell(a[8]) + ' |'
   puts '-------------'
 end
 
 def cell(value)
-  value.to_s
+  if value == 0
+    ' '
+  elsif value == 1
+    'X'
+  else
+    'O'
+  end
 end
 
-board = {tl:0,tm:0,tr:0,ml:0,mm:0,mr:0,bl:0,bm:0,br:0}
+def change_board(c,b,p)
+  if p == 1
+    b[c] = 1
+  elsif p == 2
+    b[c] = 2
+  end
+end
+
+board = [0,0,0,0,0,0,0,0,0]
+
+display_board(board)
+
+puts "Pick a square, 1-9."
+choice = gets.to_i-1
+
+change_board(choice, board, 1)
 
 display_board(board)
