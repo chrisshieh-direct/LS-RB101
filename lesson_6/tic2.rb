14d3167cc33ff68f5ae56a7b818ac65b80bc3e72
Chris Shieh <chrisshieh.direct@gmail.com>
2022-09-28 20:31:47 -0600

    basic version done pre-instruction

--- lesson_6/tic2.rb
+++ lesson_6/tic2.rb
@@ -1,14 +1,25 @@
-WINS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+WINS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+  [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+]
 
 def display_board(a)
   system('clear')
+  line
+  row(a, 0, 1, 2)
+  line
+  row(a, 3, 4, 5)
+  line
+  row(a, 6, 7, 8)
+  line
+end
+
+def line
   puts '-------------'
-  puts '| ' + cell(a[0]) + ' | ' + cell(a[1]) + ' | ' + cell(a[2]) + ' |'
-  puts '-------------'
-  puts '| ' + cell(a[3]) + ' | ' + cell(a[4]) + ' | ' + cell(a[5]) + ' |'
-  puts '-------------'
-  puts '| ' + cell(a[6]) + ' | ' + cell(a[7]) + ' | ' + cell(a[8]) + ' |'
-  puts '-------------'
+end
+
+def row(a2, x, y, z)
+  puts '| ' + cell(a2[x]) + ' | ' + cell(a2[y]) + ' | ' + cell(a2[z]) + ' |'
 end
 
 def cell(value)
@@ -21,7 +32,7 @@ def cell(value)
   end
 end
 
-def change_board(c,b,p,rec)
+def change_board(c, b, p, rec)
   if p == 1
     b[c] = 1
     rec << c
@@ -31,8 +42,8 @@ def change_board(c,b,p,rec)
   end
 end
 
-def check_winner(board,rec,player)
-  if WINS.any?{ |y| y.all?{ |x|rec.include?(x)} }
+def check_winner(rec, player)
+  if WINS.any? { |y| y.all? { |x| rec.include?(x) } }
     if player == 1
       puts "You WIN!"
     else
@@ -51,11 +62,11 @@ def check_tie(board)
   true
 end
 
-board = [0,0,0,0,0,0,0,0,0]
+board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 record = []
 comp_record = []
 
-#MAIN LOOP
+# MAIN LOOP
 loop do
   display_board(board)
 
@@ -64,7 +75,7 @@ loop do
 
     loop do
       puts "Pick a square, 1-9."
-      choice = gets.to_i-1
+      choice = gets.to_i - 1
       if board[choice] != 0
         puts "Sorry, that's occupied. Pick again."
         next
@@ -77,7 +88,7 @@ loop do
 
     display_board(board)
 
-    break if check_winner(board,record,1)
+    break if check_winner(record, 1)
     break if check_tie(board)
 
     puts "Now the computer will pick a square."
@@ -97,14 +108,14 @@ loop do
 
     display_board(board)
 
-    break if check_winner(board,comp_record,2)
+    break if check_winner(comp_record, 2)
     break if check_tie(board)
   end
 
   puts "Play again?"
   again = gets.chomp.downcase
   if again == "y"
-    board = [0,0,0,0,0,0,0,0,0]
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     record = []
     comp_record = []
     next
