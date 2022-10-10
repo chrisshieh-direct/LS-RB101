814051b05f24435f422d3d16c25c4ef32ebb9859
Chris Shieh <chrisshieh.direct@gmail.com>
2022-10-09 23:09:57 -0700

    initial rubocop cleanup

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -3,9 +3,9 @@ require 'pry'
 
 RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 SUITS = ['C', 'H', 'S', 'D']
-VALUES = {'A' => 11, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
-          '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 
-          'Q' => 10, 'K' => 10}
+VALUES = { 'A' => 11, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
+           '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10,
+           'Q' => 10, 'K' => 10 }
 
 def display_table(dlr, plr)
   system 'clear'
@@ -116,7 +116,7 @@ loop do
   puts "Dealer total: " + total(dealer).to_s
   puts "Dealer busted." if bust?(dealer)
 
-  msg = (total(player) > total(dealer)) ? "You win." : "Dealer wins."
+  msg = total(player) > total(dealer) ? "You win." : "Dealer wins."
   puts msg
 
   puts "Play again?"
