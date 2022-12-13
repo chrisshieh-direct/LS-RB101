6b2a92866dbcf52f059785f2a5f3e11b4bb2e511
Chris Shieh <dev@chrisgrace.com>
2022-12-13 12:11:57 -0800

    done with 109

--- 109.rb
+++ 109.rb
@@ -1,9 +1,17 @@
-mystery_object_1 = :boot
-mystery_object_2 = :crab
+require 'prime'
 
-p mystery_object_1 <=> mystery_object_2 #=> -1
+def gap(g, m, n)
+  possibles = Prime.each(n).select { |x| x >= m }
+  possibles.each_cons(2) do |a, b|
+    if (a - b).abs == g
+      return [a, b]
+    end
+  end
+  nil
+end
 
-mystery_object_1 = 1.3
-mystery_object_2 = 2.0
-
-p mystery_object_1 <=> mystery_object_2 #=> -1
+p gap(2, 100, 110) == [101, 103]
+p gap(4,100,110) == [103, 107]
+p gap(6,100,110) == nil
+p gap(8,300,400) == [359, 367]
+p gap(10,300,400) == [337, 347]
