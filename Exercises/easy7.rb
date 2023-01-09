59efb73b35d70a20d21c8d0728406e470b58d7fb
Chris Shieh <dev@chrisgrace.com>
2023-01-08 18:29:19 -0800

    cleanup

--- Exercises/easy7.rb
+++ Exercises/easy7.rb
@@ -1,7 +1,22 @@
-def substrings(s)
-  0.upto(s.length - 1).flat_map do |start| 
-    1.upto(s.length - start).map do |length| 
-      s[start, length]
-    end
-  end.uniq
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+  'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+  'flow', 'neon']
+
+sorted = words.map do |word|
+  word.chars.sort.join
+end
+
+anagrams = {}
+
+sorted.uniq.each do |sortword|
+  anagrams[sortword] = []
+end
+
+words.each do |word|
+  key = word.chars.sort.join
+  anagrams[key] << word
+end
+
+anagrams.each do |k, v|
+  puts "Letters in '#{k}' anagram to: #{v.sort}"
 end
